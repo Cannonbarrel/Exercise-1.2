12ac951fd9b855a3da014b05ae1d98d465568376
Cannonbarrel <Firenoob1001@gmail.com>
2024-05-27 02:58:23 +0000

    took away the comments

--- main.cs
+++ main.cs
@@ -1,13 +1,13 @@
 using System;
 
-class Lyrics
+class Program
 {
     static void Main(string[] args)
     {
-        // Display lyrics
-        Console.WriteLine("Oh, well imagine");
-        Console.WriteLine("As I'm pacing the pews in a church corridor");
-        Console.WriteLine("And I can't help but to hear");
-        Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+        Console.WriteLine("Now, this is a story all about how");
+        Console.WriteLine("My life got flipped-turned upside down");
+        Console.WriteLine("And I'd like to take a minute");
+        Console.WriteLine("Just sit right there");
+        Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
     }
 }
