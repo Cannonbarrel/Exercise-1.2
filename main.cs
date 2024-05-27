098de071fd6d689a0131886d0e0cb6860324ae9f
Cannonbarrel <Firenoob1001@gmail.com>
2024-05-27 00:23:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define personal information
+        string name = "Lucinda Potter";
+        string birthdate = "6/24/1992";
+        string workPhone = "000-000-0101";
+        string cellPhone = "000-000-0189";
+
+        // Display personal information
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+    }
+}
